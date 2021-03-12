7ec89875f46e6c5897080510f00b1df6a6ca029e
Torquato <Torquato@DESKTOP-49LM9QF>
2021-03-12 04:26:14 -0300

    ThreadEx3

---
+++ src/controller/Thread1.java
@@ -0,0 +1,24 @@
+package controller;
+
+public class Thread1 extends Thread{
+	
+	private int matriz[];
+	
+	public Thread1(int[] matriz) {
+		this.matriz = matriz;
+		
+	}
+	
+	public void run() {
+		soma();
+	}
+	
+	private void soma() {
+		int soma = 0;
+		
+		for(int i = 0; i < matriz.length; i++ ) {
+			soma = matriz[i] + soma;
+		}
+		System.out.println("A soma da linha 1 é: "+soma);
+	}
+}
---
+++ src/view/Main.java
@@ -0,0 +1,29 @@
+package view;
+
+import controller.Thread1;
+import controller.Thread2;
+import controller.Thread3;
+
+public class Main {
+	public static void main(String[] args) {
+
+		int matriz[][] = new int[3][5];
+
+		for (int i = 0; i < 3; i++) {
+
+			for (int x = 0; x <5; i++) {
+				matriz[i][x] = (int) Math.random();
+			}
+		}
+		
+		Thread1 t = new Thread1(matriz[1]);
+		t.start();
+
+		Thread2 dois = new Thread2(matriz[2]);
+		dois.start();
+
+		Thread3 tres = new Thread3(matriz[3]);
+		tres.start();
+		
+	}
+}
